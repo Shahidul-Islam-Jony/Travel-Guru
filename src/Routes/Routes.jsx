b8976840e6db8183cf29e295cfc834f585debacb
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-05 21:19:16 +0600

    React router setup with firebase auth

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+
+import { createBrowserRouter } from "react-router-dom"
+import Root from "../layouts/Root";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Root></Root>,
+        children:[
+            {
+                
+            }
+        ]
+    }
+])
+
+export default router;
